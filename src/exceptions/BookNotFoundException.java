3637e27ee5470273cdeeff6ed6e6bf9ef43a7bbf
Loi3110 <138375454+Loi3110@users.noreply.github.com>
2026-06-05 17:32:41 +0700

    Add BookNotFoundException class

---
+++ src/exceptions/BookNotFoundException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class BookNotFoundException extends Exception {
+    public BookNotFoundException(String message) {
+        super(message);
+    }
+
+    public BookNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
