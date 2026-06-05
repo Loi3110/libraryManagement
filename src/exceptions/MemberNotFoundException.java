873754198f12357f3a421f3f2c01123306347f4a
Loi3110 <138375454+Loi3110@users.noreply.github.com>
2026-06-05 17:33:26 +0700

    Add MemberNotFoundException class

---
+++ src/exceptions/MemberNotFoundException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public class MemberNotFoundException extends Exception {
+    public MemberNotFoundException(String message) {
+        super(message);
+    }
+
+    public MemberNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
